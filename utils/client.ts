982b51fefa1fe5266f5ea65c854fd6889f31874f
Choxy5 <kristijan.blazevic000@gmail.com>
2022-07-17 11:44:52 +0200

    Main Video View

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: '12dse7j2',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
